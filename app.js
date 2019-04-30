fd1de58bb22aab3aa7bed2da617bc407ee8b5ee3
Ernest Ibeh <ibeh_ernest@yahoo.com>
2019-04-30 10:19:57 +0100

    Resolving error 127

--- app.js
+++ app.js
@@ -5,17 +5,8 @@ const morgan = require('morgan')
 app.use(morgan('dev'))
 
 app.use((req, res, next) => {
-  const error = new Error('Not Found')
-  error.status = 404
-  next(error)
-})
-
-app.use((error, req, res, next) => {
-  res.status(error.status || 500)
-  res.json({
-    error: {
-      message: error.message
-    }
+  res.status(200).json({
+    message: 'It works!'
   })
 })
 
