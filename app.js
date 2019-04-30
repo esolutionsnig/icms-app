4c33af1b42a8b8ee35f7e1464b28700ac66e7b2d
Ernest Ibeh <ibeh_ernest@yahoo.com>
2019-04-30 10:05:21 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+
+app.use(morgan('dev'))
+
+app.use((req, res, next) => {
+  const error = new Error('Not Found')
+  error.status = 404
+  next(error)
+})
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500)
+  res.json({
+    error: {
+      message: error.message
+    }
+  })
+})
+
+module.exports = app
