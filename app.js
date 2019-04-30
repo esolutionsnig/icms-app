1ff3a9248c831d9b4f49ae3f07eb9bf8f2611c73
Ernest Ibeh <ibeh_ernest@yahoo.com>
2019-04-30 11:10:48 +0100

    Added Body Parser and CORS handler

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
 
 app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, GET, DELETE')
+    return res.status(200).json({})
+  }
+  next()
+})
 
 app.use((req, res, next) => {
   const error = new Error('Not Found')
