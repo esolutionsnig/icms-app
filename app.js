09392cbc7a99538952eab842d7efa7a404f9b2af
Ernest Ibeh <ibeh_ernest@yahoo.com>
2019-04-30 10:25:14 +0100

    Resolved Issue

--- app.js
+++ app.js
@@ -5,8 +5,17 @@ const morgan = require('morgan')
 app.use(morgan('dev'))
 
 app.use((req, res, next) => {
-  res.status(200).json({
-    message: 'It works!'
+  const error = new Error('Not Found')
+  error.status = 404
+  next(error)
+})
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500)
+  res.json({
+    error: {
+      messsage: error.messsage
+    }
   })
 })
 
